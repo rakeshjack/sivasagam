8d45a9f1f1ca1d140be691ecff493decd08896ab
Rakesh <rakeshjack7@gmail.com>
2025-05-28 18:51:12 +0530

    categories api

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use App\Models\Categorie;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +16,11 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
+
+Route::get('categories', function() {
+    $categories = Categorie::with('catimages')->get();
+    return response()->json(['message' => 'devotional lists','user' =>$categories], 201);
+});
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
